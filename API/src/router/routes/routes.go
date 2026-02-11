143947e584f036596f190fa65486cdf6a996b924
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-02-11 12:43:44 -0300

    implementacao da funcao configurar para colocar todas as rotas dentro do router

--- API/src/router/routes/routes.go
+++ API/src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route representa todas as rotas da API
 type Route struct {
@@ -8,4 +12,14 @@ type Route struct {
 	Method       string
 	Handler      func(http.ResponseWriter, *http.Request)
 	RequiresAuth bool
+}
+
+// configurar coloca todas as rotas dentro do router
+func Configure(r *mux.Router) *mux.Router {
+	routes := routesUsers
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Handler).Methods(route.Method)
+	}
+	return r
 }
