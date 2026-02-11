9f5906ae75fb5dea887a70bb95f8240ab3f7db62
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-02-11 11:55:19 -0300

    Criacao da pasta router

--- API/main.go
+++ API/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gasparguilherme/Netwise/api/src/router"
+)
 
 func main() {
 	fmt.Println("Rodando API!")
+
+	r := router.Gerar()
+
+	log.Fatal((http.ListenAndServe(":5000", r)))
 }
